5aeab799e1b738117246fa9202887139e6b79ecb
AkihiroKagimoto <smartphoneyou15@gmail.com>
2018-08-12 15:01:27 +0900

    create_blog

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,20 @@
+class BlogsController < ApplicationController
+  def index
+    
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+ def edit
+ end
+
+ def create
+ end
+
+ def destroy
+ end
+end
---
+++ db/migrate/20180812053227_blogs_create.rb
@@ -0,0 +1,4 @@
+class BlogsCreate < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
